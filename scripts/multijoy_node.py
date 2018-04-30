82cd473bc74df4f527202c52eeac73520319f33d
cmower <chris.mower@ed.ac.uk>
2018-04-30 08:13:52 +0100

    [UNTESTED] updates for new MultiJoy.msg

--- scripts/multijoy_node.py
+++ scripts/multijoy_node.py
@@ -1,41 +1,35 @@
 #!/usr/bin/env python
 import rospy
 import message_filters as mf
-import multijoy.msg as mj
+from multijoy.msg import MultiJoy
 from sensor_msgs.msg import Joy
 
-class MultiJoy(object):
+class MultiJoyParser(object):
     def __init__(self):
-        
+
+        # Retrieve parameters
         if rospy.has_param('debug'):
             self.debug=rospy.get_param('debug')
         else:
             self.debug=False
-        
-        self.njoy=rospy.get_param('njoy')
+        self.njoys=rospy.get_param('njoys')
 
-        Msg='mj.MultiJoy{}'.format(self.njoy)
-        self.msg=eval('{}()'.format(Msg))
+        # Setup ros publisher
+        self.multijoy_pub=rospy.Publisher('/multijoy', MultiJoy, queue_size=10)
 
-        self.pub=rospy.Publisher('multijoy', eval(Msg), queue_size=10)
-        
-        self.subs=[None]*self.njoy
-        for i in xrange(self.njoy):
-            topic='joy/{}'.format(i+1)
-            self.subs[i]=mf.Subscriber(topic, Joy)
+        # Setup ros subscribers
+        self.joy_subs=[mf.Subscriber('/joy/'+str(i),Joy) for i in xrange(self.njoys)]
         self.timeSync=mf.ApproximateTimeSynchronizer(self.subs, 10, self.njoy*100)
         self.timeSync.registerCallback(self.update)
-            
+
     def update(self, *args):
-        for i, arg in enumerate(args):
-            attr='joy{}'.format(i+1)
-            setattr(self.msg, attr, arg)
-        self.msg.header.stamp=rospy.Time.now()
-        if self.debug:
-            print self.msg
-        self.pub.publish(self.msg)
+        msg=MultiJoy()
+        msg.header.stamp=rospy.Time.now()
+        msg.njoys=len(args)
+        msg.joys=args
+        self.multijoy_pub.publish(msg)
 
 if __name__=='__main__':
     rospy.init_node('multijoy_node')
-    mj=MultiJoy()
+    parser=MultiJoyParser()
     rospy.spin()
