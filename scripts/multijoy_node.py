e07d088d2d632718f5c85bec47719efa0b978cf8
Chris Mower <cmower@users.noreply.github.com>
2018-04-30 09:09:19 +0100

    Use array in ros msg (#1)
    * [UNTESTED] updates for new MultiJoy.msg
    
    * [WORKING] updated for new MultiJoy.msg

--- scripts/multijoy_node.py
+++ scripts/multijoy_node.py
@@ -1,41 +1,47 @@
 #!/usr/bin/env python
 import rospy
 import message_filters as mf
-import multijoy.msg as mj
+from multijoy.msg import MultiJoy
 from sensor_msgs.msg import Joy
 
-class MultiJoy(object):
+class MultiJoyParser(object):
+    
     def __init__(self):
-        
-        if rospy.has_param('debug'):
-            self.debug=rospy.get_param('debug')
+
+        # Retrieve parameters
+        self.ns=rospy.get_name()
+        self.param_name_debug=self.ns+'/debug'
+        self.param_name_njoys=self.ns+'/njoys'
+        if rospy.has_param(self.param_name_debug):
+            self.debug=rospy.get_param(self.param_name_debug)
         else:
             self.debug=False
-        
-        self.njoy=rospy.get_param('njoy')
+        self.njoys=rospy.get_param(self.param_name_njoys)
 
-        Msg='mj.MultiJoy{}'.format(self.njoy)
-        self.msg=eval('{}()'.format(Msg))
-
-        self.pub=rospy.Publisher('multijoy', eval(Msg), queue_size=10)
-        
-        self.subs=[None]*self.njoy
-        for i in xrange(self.njoy):
-            topic='joy/{}'.format(i+1)
-            self.subs[i]=mf.Subscriber(topic, Joy)
-        self.timeSync=mf.ApproximateTimeSynchronizer(self.subs, 10, self.njoy*100)
-        self.timeSync.registerCallback(self.update)
-            
-    def update(self, *args):
-        for i, arg in enumerate(args):
-            attr='joy{}'.format(i+1)
-            setattr(self.msg, attr, arg)
-        self.msg.header.stamp=rospy.Time.now()
         if self.debug:
-            print self.msg
-        self.pub.publish(self.msg)
+            rospy.loginfo('debug={}'.format(self.debug))
+            rospy.loginfo('njoys={}'.format(self.njoys))
+            
+        # Setup ros publisher
+        self.multijoy_pub=rospy.Publisher('/multijoy', MultiJoy, queue_size=10)
+
+        # Setup ros subscribers
+        self.joy_subs=[mf.Subscriber('/joy/'+str(i),Joy) for i in xrange(self.njoys)]
+        self.timeSync=mf.ApproximateTimeSynchronizer(self.joy_subs, 10, self.njoys*100)
+        self.timeSync.registerCallback(self.update)
+
+
+    def update(self, *args):
+        msg=MultiJoy()
+        msg.header.stamp=rospy.Time.now()
+        msg.njoys.data=self.njoys
+        msg.joys=args
+        self.multijoy_pub.publish(msg)
+
+        if self.debug:
+            rospy.loginfo('joys retrieved and published')
 
 if __name__=='__main__':
     rospy.init_node('multijoy_node')
-    mj=MultiJoy()
+    parser=MultiJoyParser()
     rospy.spin()
